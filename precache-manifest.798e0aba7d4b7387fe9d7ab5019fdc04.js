6004dac9ef65934a155ddf36061de1293ab4eb8e
aibar.baktygaliyev <aibar.baktygaliyev@nu.edu.kz>
2021-12-24 15:45:22 +0600

    deploy

---
+++ precache-manifest.798e0aba7d4b7387fe9d7ab5019fdc04.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acab84b7073e0be6c36ba8445ea13a07",
+    "url": "/medical-school/android-chrome-192x192.png"
+  },
+  {
+    "revision": "9cdeb82604bbdc4e9c48bb710c1994b2",
+    "url": "/medical-school/android-chrome-512x512.png"
+  },
+  {
+    "revision": "e533e2745f7d0899f95d177030ebfcb2",
+    "url": "/medical-school/apple-touch-icon.png"
+  },
+  {
+    "revision": "a493ba0aa0b8ec8068d786d7248bb92c",
+    "url": "/medical-school/browserconfig.xml"
+  },
+  {
+    "revision": "cb3d77d3fe396ced2af7",
+    "url": "/medical-school/css/app.bdf85361.css"
+  },
+  {
+    "revision": "9838862ae5666619135e",
+    "url": "/medical-school/css/chunk-vendors.5d3035bc.css"
+  },
+  {
+    "revision": "7057e08d483d4c4d62415cb7e98df1fb",
+    "url": "/medical-school/favicon-16x16.png"
+  },
+  {
+    "revision": "70406c63a24a413c044b8feab5c1446a",
+    "url": "/medical-school/favicon-32x32.png"
+  },
+  {
+    "revision": "219b4b919bf48de75a618c6b9bb68b18",
+    "url": "/medical-school/index.html"
+  },
+  {
+    "revision": "cb3d77d3fe396ced2af7",
+    "url": "/medical-school/js/app.76cf649b.js"
+  },
+  {
+    "revision": "9838862ae5666619135e",
+    "url": "/medical-school/js/chunk-vendors.d73cb5e0.js"
+  },
+  {
+    "revision": "03941e7d7851d810802106b7754504a1",
+    "url": "/medical-school/manifest.json"
+  },
+  {
+    "revision": "f23fbb417b026e10e94181961a7ca0c6",
+    "url": "/medical-school/mstile-150x150.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/medical-school/robots.txt"
+  },
+  {
+    "revision": "7e999c9127a5f24db92c285e5795c507",
+    "url": "/medical-school/safari-pinned-tab.svg"
+  },
+  {
+    "revision": "b9aa277fcfc34c31db6c7a7ea3469b8c",
+    "url": "/medical-school/site.webmanifest"
+  }
+]);
