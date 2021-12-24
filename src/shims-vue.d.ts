53befb27d0a7da929869eee1ea0c85b4de0fb9a3
aibar.baktygaliyev <aibar.baktygaliyev@nu.edu.kz>
2021-12-24 11:49:11 +0600

    [UPDATED] 24.12.2021

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -2,3 +2,6 @@ declare module '*.vue' {
     import Vue from 'vue'
     export default Vue
 }
+
+declare module 'crypto-js/sha256'
+declare module 'crypto-js/enc-base64'
--- src/views/Index.vue
+++ src/views/Index.vue
@@ -15,8 +15,18 @@
 
 <script lang="ts">
 import Vue from 'vue'
+import SHA256 from 'crypto-js/sha256'
+import Base64 from 'crypto-js/enc-base64'
+
 export default Vue.extend({
-    name: 'MainContainer'
+    name: 'MainContainer',
+    mounted: function() {
+        // console.log(AES('password'))
+        const result = Base64.stringify(SHA256('password'))
+        console.log(result)
+        console.log(result === 'XohImNooBHFR0OVvjcYpJ3NgPQ1qq73WKhHvch0VQtg=')
+
+    }
 })
 </script>
 
