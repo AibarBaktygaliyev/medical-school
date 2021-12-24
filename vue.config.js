45351f4d9a29ea33c145a55c905b172df98eb3c9
aibar.baktygaliyev <aibar.baktygaliyev@nu.edu.kz>
2021-12-24 12:31:03 +0600

    [UPDATED] 24.12.2021

---
+++ vue.config.js
@@ -0,0 +1,31 @@
+var merge = require('webpack-merge')
+
+module.exports = {
+    publicPath: 'medical-school',
+    pwa: {
+        workboxOptions: {
+            skipWaiting: true
+        }
+    },
+    chainWebpack: config => {
+        config.module
+            .rule('svg')
+            .use('html-loader')
+            .loader('html-loader')
+        config.module
+            .rule('vue')
+            .use('vue-loader')
+            .loader('vue-loader')
+            .tap(options =>
+                merge(options, {
+                    transformAssetUrls: {
+                        video: ['src', 'poster'],
+                        source: 'src',
+                        img: 'src',
+                        image: 'xlink:href',
+                        'vl-style-icon': 'src',
+                    },
+                })
+            )
+    }
+}
